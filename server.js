2ef27cdd1f98f4575ef573ce8185b8df1d7e7c02
David Rodríguez <devdrod@gmail.com>
2020-05-01 12:16:36 -0600

    cors modified json proxy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({extended:false}));
 
 var corsOptions = {
     origin: 'https://davidrodriguezdev.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200 
 }
 
 app.use(cors(corsOptions));
