1dd6bf6734a789f618449339c68d543c0b14dc14
David Rodríguez <devdrod@gmail.com>
2020-05-04 14:01:15 -0600

    server modil

--- server.js
+++ server.js
@@ -8,54 +8,19 @@ connectDB()
 
 app.get('/',(req,res)=> res.send('API Running'));
 
-
-
-
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://davidrodriguezdev.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
-
-
-
 // init middleware
 
 app.use(express.json({extended:false}));
 
-// Set up a whitelist and check against it:
-var whitelist = ['http://davidrodriguezdev.com', 'https://myportfoliobackend2020.herokuapp.com', 'https://devdrod.github.io/Reactscroll-jrportfolio/' ]
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+    origin: 'https://davidrodriguezdev.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-// Then pass them to cors:
-app.use('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Define Routes
-app.use('/api/contact', require('./routes/api/contact'))
+app.use('/api/contacts', require('./routes/api/contacts'))
 
 const PORT = process.env.PORT || 5000;
 
