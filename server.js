d98c503a3bc94f6ec8eb373b33fee78ce3506ab5
David Rodríguez <devdrod@gmail.com>
2020-05-04 13:08:02 -0600

    server mod

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors(corsOptions));
 
 // Define Routes
 app.use('/api/contacts', require('./routes/api/contacts'))
-
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`server started on port${PORT}`));
