277a6fbe304b97637685ccbbf767e4d68a731a5a
David Rodríguez <devdrod@gmail.com>
2020-05-04 13:12:28 -0600

    server modi

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ connectDB()
 
 app.get('/',(req,res)=> res.send('API Running'));
 
+
+
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://davidrodriguezdev.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
+
+
 // init middleware
 
 app.use(express.json({extended:false}));
@@ -29,7 +56,7 @@ app.use(cors(corsOptions));
 
 // Define Routes
 app.use('/api/contacts', require('./routes/api/contacts'))
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`server started on port${PORT}`));
