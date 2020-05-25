a21558896d581c46b36df69e73c482a6ffde5239
David Rodríguez <devdrod@gmail.com>
2020-05-25 09:22:07 -0600

    server port 5000 deleted

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.use(cors(corsOptions));
 // Define Routes
 app.use('/api/contacts', require('./routes/api/contacts'))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, ()=> console.log(`server started on port${PORT}`));
